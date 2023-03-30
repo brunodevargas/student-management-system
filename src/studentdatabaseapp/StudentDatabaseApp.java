daeda7ee00b03563984cb57a6f25d4aefff077df
Bruno Vargas <brunocalegaridevargas@gmail.com>
2023-03-29 21:11:42 -0300

    started class student

---
+++ src/studentdatabaseapp/StudentDatabaseApp.java
@@ -0,0 +1,9 @@
+package studentdatabaseapp;
+
+public class StudentDatabaseApp {
+    public static void main(String[] args) {
+        // Ask how many users we want to add
+
+        // Create n numbers of new students
+    }
+}
