ca6205bc117dae7d04e1ae57d40afa07951ce24a
Bruno Vargas <brunocalegaridevargas@gmail.com>
2023-04-12 15:04:49 -0300

    new methods: setStudID and courseEnrollment

--- src/studentdatabaseapp/Student.java
+++ src/studentdatabaseapp/Student.java
@@ -6,13 +6,14 @@ public class Student {
     private String firstName;
     private String lastName;
     private String gradeYear;
-    private int studentID;
-    private String courses;
-    private int tuitionBalance;
-    private int costOfCourse = 600;
+    private String studentID;
+    private String courses = null;
+    private int tuitionBalance = 0;
+    private static int costOfCourse = 600;
+    private static int id = 1000;
 
     // Constructor: prompt user to enter student´s name and year
-    public Student(){
+    public Student() {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter student first name: ");
         this.firstName = in.nextLine();
@@ -22,15 +23,41 @@ public class Student {
 
         System.out.println("1 - Freshman\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student class level: ");
         this.gradeYear = in.nextLine();
+
+        setStudentID();
+        System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
     }
 
     // Generate  an ID
+    private void setStudentID() {
+        // Grade level + ID
+        id++;
+        this.studentID = gradeYear + "" + id;
+    }
 
     // Enroll in courses
+    public void enroll() {
+        do {
+            System.out.print("Enter course to enroll (Q to quit): ");
+            Scanner in = new Scanner(System.in);
+            String course = in.nextLine();
+            if (!course.equals("Q")) {
+                courses = courses + "\n" + course;
+                tuitionBalance = tuitionBalance + costOfCourse;
+            }
+            else {
+                break;
+                }
+            } while (1 != 0);
 
-    // View balance
+            System.out.println("ENROLLED IN: " + courses);
+            System.out.println("TUITION BALANCE: " + tuitionBalance);
+        }
 
-    // Pay Tuition
+        // View balance
 
-    // Show status
-}
+        // Pay Tuition
+
+        // Show status
+    }
+
